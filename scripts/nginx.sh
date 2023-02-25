3f8b28d70b2d03afdc46f48c35298e5fb96133c7
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-26 00:20:08 +0530

    q

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,7 +1,8 @@
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
 
-filepath here
+sudo apt install nginx
+
 sudo cp -rf /var/lib/jenkins/workspace/Django-Admin/nginx/nginx.conf /etc/nginx/sites-available/Django-Admin
 chmod 710 /var/lib/jenkins/workspace/Django-Admin
 
