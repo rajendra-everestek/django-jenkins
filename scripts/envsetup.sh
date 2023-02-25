849ab8f124eaecb70550765865ae6e27e1f2eaf5
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-25 23:07:31 +0530

    q

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -6,4 +6,10 @@ python3 -m venv /var/lib/jenkins/workspace/Django-Admin/env
 
 source /var/lib/jenkins/workspace/Django-Admin/env/bin/activate
 
-pip install django
+pip install -r /var/lib/jenkins/workspace/Django-Admin/requirements.txt
+
+pip install gunicorn
+
+cd /var/lib/jenkins/workspace/Django-Admin/djangojenkin
+
+/var/lib/jenkins/workspace/Django-Admin/env/bin/gunicorn --bind 0.0.0.0:8000 /var/lib/jenkins/workspace/Django-Admin/djangojenkin/djangojenkin.wsgi
