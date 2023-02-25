608cfadb8863a73c4c1d082ebd87c9866f20a2ed
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-26 04:21:26 +0530

    b

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -2,6 +2,6 @@
 sudo cp /home/ubuntu/django-jenkins/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
 sudo cp /home/ubuntu/django-jenkins/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
-# systemctl daemon-reload
-# sudo systemctl start gunicorn.service
-# sudo systemctl enable gunicorn.service
+systemctl daemon-reload
+sudo systemctl start gunicorn.service
+sudo systemctl enable gunicorn.service
