dfcf3999346dfc14776edd83ca99fdbe85ad9540
rajendrabadgujar <rajendra.badgujar@everestek.com>
2023-02-26 15:49:40 +0530

    q

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-# sudo cp /home/ubuntu/django-jenkins/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
-# sudo cp /home/ubuntu/django-jenkins/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
+sudo cp /home/ubuntu/django-jenkins/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
+sudo cp /home/ubuntu/django-jenkins/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
-# sudo systemctl daemon-reload
-# sudo systemctl start gunicorn.service
-# sudo systemctl enable gunicorn.service
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn.service
+sudo systemctl enable gunicorn.service
 
 # gunicorn -c /home/ubuntu/django-jenkins/gunicorn/gunicorn_config.py djangojenkin.wsgi:application
-/home/ubuntu/env/bin/gunicorn -c /home/ubuntu/django-jenkins/gunicorn/gunicorn_config.py djangojenkin.wsgi
+# /home/ubuntu/env/bin/gunicorn -c /home/ubuntu/django-jenkins/gunicorn/gunicorn_config.py djangojenkin.wsgi
 
 sudo service nginx start
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -6,6 +6,8 @@
 sudo cp -rf /home/ubuntu/django-jenkins/nginx/nginx.conf /etc/nginx/sites-available/djangojenkin
 cd /etc/nginx/sites-enabled
 
+sudo rm -rf /etc/nginx/sites-enabled/djangojenkin 
+
 sudo ln -s /etc/nginx/sites-available/djangojenkin
 
 ls -ln
